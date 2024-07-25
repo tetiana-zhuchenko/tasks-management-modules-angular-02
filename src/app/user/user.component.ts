4233bf4f9dbc8fb0981c20ee39981c70cccef26d
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-25 21:19:57 +0300

    feat: init project with "NgModule"

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  imports: [CardComponent],
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
